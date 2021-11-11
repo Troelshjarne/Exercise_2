9ff55025ad1ca1b88a307757909f838a86376e0a
troels <trhj@itu.dk>
2021-11-11 11:36:49 +0100

    proto setup and some node code

---
+++ nodes/node.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	criticalpackage "github.com/Troelshjarne/Exercise_2/critical"
+	//"google.golang.org/grpc"
+)
+
+var lamTime = 0
+
+type Server struct {
+	criticalpackage.UnimplementedCommunicationServer
+
+	channel map[string][]chan *criticalpackage.Request
+}
+
+func (s *Server) joinCluster(ch *criticalpackage.Channel, requestStream criticalpackage.Communication_JoinClusterClient) error {
+	requestChannel := make(chan *criticalpackage.Request)
+
+	s.channel[ch.NodeId] = append(s.channel[ch.NodeId], requestChannel)
+}
