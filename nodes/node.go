d0cfebff1b57c1db4429865a92f9dbb0dc4ed74d
troels <trhj@itu.dk>
2021-11-16 10:49:19 +0100

    .

--- nodes/node.go
+++ nodes/node.go
@@ -23,15 +23,6 @@ type Server struct {
 	channel map[string][]chan *criticalpackage.Request
 }
 
-func (s *Server) joinCluster(ch *criticalpackage.Channel, requestStream criticalpackage.Communication_JoinClusterClient) error {
-	requestChannel := make(chan *criticalpackage.Request)
-
-	s.channel[ch.NodeId] = append(s.channel[ch.NodeId], requestChannel)
-
-}
-
-func (s *Server) sendRequest()
-
 func main() {
 
 	fmt.Println("=== Node starting up ===")
